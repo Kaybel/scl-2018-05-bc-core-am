c21107bc67f88e543339ffb0b3a86d98c9ff1f18
Kaybel <kblmrtnz@gmail.com>
2018-05-27 16:59:17 -0400

    boton medio funcional

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-/* Acá va tu código */
+let cifrado = cuadroTextoCifrado;
+let descifrado = cuadroTextoDescifrado;
+function myFunction() {
+    document.getElementById("cuadroTextoCifrado").value = document.getElementById("cuadroTextoDescifrado").value;
+}
+function myFunction() {
+    document.getElementById("cuadroTextoDescifrado").value = document.getElementById("cuadroTextoCifrado").value;
+}
