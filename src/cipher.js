100d22db819e032f57af145168d30e857f9280a3
Kaybel <kblmrtnz@gmail.com>
2018-05-28 13:59:14 -0400

    Ambos cuadros de texto cambian la informacion segun se presione boton correspondiente.

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,7 @@
 window.cipher = {
   encode: () => {
     /* Acá va tu código */
+    // es un obj
   },
   decode: () => {
     /* Acá va tu código */
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
     // 2. obtener la cantidad de espacios y meterlo en una variable
     // 3. llamar a la funcion que cifra que tengo en cipher.js y almanecar ese resultado
     // 4. meter el resultado del paso 2 en la caja donde corresponda
-
+/*
     //1
 function cifrar() {
     let textoACifrar = document.getElementById("cuadroTextoCifrado").value;
@@ -12,14 +12,30 @@ function cifrar() {
 }
 function descifrar() {
     let textoADescifrar = document.getElementById("cuadroTextoDescifrado").value;
+    //3
+    // window.cipher.encode puedo llamar a la funcion en cipher.js
     let textoDescifrado = window.cipher.decode();
 }
    // 2
-
 function space(){
-    let inputNumber = document.getElementById("numInput").value;
+    let inputNumber = parseInt(document.getElementById("numInput").value , 10);
   }
 
 let cifrado = cifrar();
 let descifrado = descifrar();
-let numSpace = space();
+let numSpace =space();
+*/
+    function cifrar() {
+         document.getElementById("cuadroTextoCifrado").value = document.getElementById("cuadroTextoDescifrado").value;
+           // window.cipher.encode puedo llamar a la funcion en cipher.js
+    let textoDescifrado = window.cipher.encode();
+     }
+    function descifrar() {
+         document.getElementById("cuadroTextoDescifrado").value = document.getElementById("cuadroTextoCifrado").value;
+           // window.cipher.encode puedo llamar a la funcion en cipher.js
+    let textoDescifrado = window.cipher.decode();
+    } 
+    
+
+    let cifrado = cifrar();
+    let descifrado = descifrar();
