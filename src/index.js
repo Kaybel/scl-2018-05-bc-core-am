1f85a2abd81fa736813103c3a9e800358b165e4d
Kaybel <kblmrtnz@gmail.com>
2018-05-28 01:20:11 -0400

    variables para guardar input y functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
+    // 1. obtener el string de la caja que tiene el texto a cifrar y meterlo en una variable
+    // 2. obtener la cantidad de espacios y meterlo en una variable
+    // 3. llamar a la funcion que cifra que tengo en cipher.js y almanecar ese resultado
+    // 4. meter el resultado del paso 2 en la caja donde corresponda
+
+    //1
 function cifrar() {
-    document.getElementById("cuadroTextoCifrado").value = document.getElementById("cuadroTextoDescifrado").value;
+    let textoACifrar = document.getElementById("cuadroTextoCifrado").value;
+    //3
+    // window.cipher.encode puedo llamar a la funcion en cipher.js
+    let textoCifrado = window.cipher.encode();
 }
 function descifrar() {
-    document.getElementById("cuadroTextoDescifrado").value = document.getElementById("cuadroTextoCifrado").value;
+    let textoADescifrar = document.getElementById("cuadroTextoDescifrado").value;
+    let textoDescifrado = window.cipher.decode();
 }
+   // 2
+
+function space(){
+    let inputNumber = document.getElementById("numInput").value;
+  }
 
 let cifrado = cifrar();
 let descifrado = descifrar();
-
+let numSpace = space();
