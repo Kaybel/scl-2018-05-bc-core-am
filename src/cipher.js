e7552f22b3b9f554606857f062540875d2657fd7
Kaybel <kblmrtnz@gmail.com>
2018-05-31 11:58:09 -0400

    ahora no funcionan los botones, crisis existencial

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,18 @@
 window.cipher = {
-  encode(i = 0, i =< getElementById("cuadroTextoCifrado").length, i++) => {
-    let result = "";
-    let resultCipher = getElementById("cuadroTextoCifrado").charCodeAt(i);
-   (resultCipher - 65 + numSpace) % 26 + 65;
+  encode : (numSpace, cifrado) => {
+    for(i = 0; i < cifrado.length; i++){
+     let result = "";
+     let resultCipher = cifrado.charCodeAt[i];
    }
-};
+   return (resultCipher - 65 + numSpace) % 26 + 65;   
+  }
+}
+window.cipher = {
+  decode : (numSpace,descifrado) => {
+    for(i = 0; i < descifrado.length; i++){
+     let result = "";
+     let resultDCipher = descifrado.charCodeAt[i];
+   }
+   return (resultCipher + 65 + numSpace) % 26 + 65;   
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-    // 1. obtener el string de la caja que tiene el texto a cifrar y meterlo en una variable
-    // 2. obtener la cantidad de espacios y meterlo en una variable
-    // 3. llamar a la funcion que cifra que tengo en cipher.js y almanecar ese resultado
-    // 4. meter el resultado del paso 2 en la caja donde corresponda
-
-
-    // hago una variable para que la info de la funcion se guarde y luego deberia llamar en una let FUERA de la funcion a la funcion como tal para sustraer su informacion que DEBERIA estar almacenada en la let
-           // window.cipher.encode puedo llamar a la funcion en c
-function space(){
-    let inputNumber = document.getElementById("numInput").innerHTML.value;
-        return inputNumber;
+window.onload = function(){
+    function space(){
+        let inputNumber = document.getElementById("numInput").innerHTML.value;
+         return inputNumber;
+    } 
+    function click(){
+        document.getElementById("btnId1").innerHTML = cifrado;
+       }
+    function click2(){
+        document.getElementById("btnId2").innerHTML = descifrado;
+       }
+    const descifrado = document.getElementById("cuadroTextoDescifrado");
+    const cifrado = document.getElementById("cuadroTextoCifrado");
+    const btn1 = document.getElementById("btnId1");
+    const btn2 = document.getElementById("btnId2"); 
+    let numSpace = space();
 }
-/*
-let textoCifrado = window.cipher.encode();
-let textoDescifrado = window.cipher.decode();
-let numSpace = space(); */
