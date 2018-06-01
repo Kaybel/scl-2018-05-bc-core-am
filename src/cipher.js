07993da53245feaec40ada9862ed629102c25397
Kaybel <kblmrtnz@gmail.com>
2018-06-01 02:48:22 -0400

    crisis existencial 2.0 nada funciona; ya no hay errores en la consola, falta llamar a las funciones y ya, supuestamente yo

--- src/cipher.js
+++ src/cipher.js
@@ -1,18 +1,20 @@
 window.cipher = {
-  encode : (numSpace, cifrado) => {
-    for(i = 0; i < cifrado.length; i++){
+  encode : (espacio1, cifrado) => {
+    for(i = 0; i < cifrado; i++){
      let result = "";
-     let resultCipher = cifrado.charCodeAt[i];
+     let espacio = space();
+     let cifradoAja = (document.getElementById("cuadroTextoCifrado"));
+     let resultCipher = (cifradoAja.charCodeAt[i]);
    }
-   return (resultCipher - 65 + numSpace) % 26 + 65;   
-  }
-}
-window.cipher = {
-  decode : (numSpace,descifrado) => {
-    for(i = 0; i < descifrado.length; i++){
+   return (resultCipher - 65 + espacio) % 26 + 65;   
+  },
+  decode : (espacio,descifrado) => {
+    for(i = 0; i < descifrado; i++){
      let result = "";
-     let resultDCipher = descifrado.charCodeAt[i];
+     let espacio = space();
+     let descifradoAja = (document.getElementById("cuadroTextoDescifrado").value);
+     let resultDCipher = (descifradoAja.offSet[i]);
    }
-   return (resultCipher + 65 + numSpace) % 26 + 65;   
+   return (resultDCipher + 65 + espacio) % 26 + 65;   
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-window.onload = function(){
+window.onload = function(){ 
     function space(){
         let inputNumber = document.getElementById("numInput").innerHTML.value;
          return inputNumber;
     } 
-    function click(){
-        document.getElementById("btnId1").innerHTML = cifrado;
-       }
-    function click2(){
-        document.getElementById("btnId2").innerHTML = descifrado;
-       }
-    const descifrado = document.getElementById("cuadroTextoDescifrado");
-    const cifrado = document.getElementById("cuadroTextoCifrado");
-    const btn1 = document.getElementById("btnId1");
-    const btn2 = document.getElementById("btnId2"); 
-    let numSpace = space();
+    const btnUno = (document.getElementById("btnId1").addEventListener("click", btn1));
+    const btnDos = (document.getElementById("btnId2").addEventListener("click", btn2)); 
+
+    function btn1(btn1){
+      let btn11 = (document.getElementById("btnId1").HTMLButtonElement) 
+        return btn1.addEventListener;
+    }
+
+    function btn2(btn2){
+      let btn22 = (document.getElementById("btnId2").innerHTML)
+        return  btn2.addEventListener;
+    }
 }
