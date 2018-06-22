170eb87986bb8f08100aa0d80805c3e3df474e8e
Kaybel <kblmrtnz@gmail.com>
2018-06-22 00:01:16 -0400

    no se imprime la info no toma la informacion como un string sino como un NaN...

--- src/cipher.js
+++ src/cipher.js
@@ -1,28 +1,21 @@
-let espacioAMover = document.getElementById("numInput").value;
-let mensajeCip = document.getElementById("cuadroTextoCifrado").value;
-let mensajeDescip = document.getElementById("cuadroTextoDescifrado").value;
-
 window.cipher = {
   encode : (cifradociph, espacioAMover) => {
-    let cifrado = mensajeDescip;
+    let cifrado = cifradociph;
     let result = "";
     for(let i = 0; i < cifrado.length; i++){
       // se aplica from char code para que retorno los espacios del char code at
-   let infoCip = ((descifrado.charCodeAt(i) + 65 - (espacioAMover)) % 26 + 65);
-  result += string.charCodeAt(infoCip);
+   let infoCip = ((cifrado.charCodeAt(i) + 65 - (espacioAMover)) % 26 + 65);
+  result += cifrado.charCodeAt(infoCip);
   }
   return result;  
-  }
-};
-// se coloca otra window.cipher porque arriba toma como error el decode.
-window.cipher = {
+  },
   decode : (descifradociph, espacioAMover) => {
-    let descifrado = mensajeCip;
+    let descifrado = descifradociph;
     let result2 = "";
     for(let i = 0; i < descifrado.length; i++){
       // se aplica from char code para que retorno los espacios del char code at
-   let infoDec = ((descifrado.fromCharCode(i) + 65 - (espacioAMover)) % 26 + 65);
-  result2 += string.fromCharCode(infoDec);
+   let infoDec = ((descifrado.fromCharCode(i) - 65 + (espacioAMover)) % 26 - 65);
+  result2 += descifrado.fromCharCode(infoDec);
   }
   return result2;  
   }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 window.onload = function() { 
-  // variables de botones y cuadros de texto
+  // variables de botones 
 const btnUno = document.getElementById("btnId1");
-const btnDos = document.getElementById("btnId2"); 
-let mensajeCip = document.getElementById("cuadroTextoCifrado").value;
-let mensajeDescip = document.getElementById("cuadroTextoDescifrado").value;
+const btnDos = document.getElementById("btnId2");
 
 // evento DOM 
 
 // llamar funciones encode y decode
 
 btnUno.addEventListener("click", () => {
-console.log('este boton cifra');
-mensajeDescip.value = mensajeCip.value.innerText;
+  let mensajeCip = document.getElementById("cuadroTextoCifrado").value;
+  let espacioAMover = document.getElementById("numInput").value; 
+  let cif = window.cipher.encode(mensajeCip, espacioAMover);
+  let str = cif.toString();
+  mensajeDescip = mensajeCip.innerText;
+  console.log(str);
+  // mensajeDescip.value = mensajeCip.value.innerText;
 });
 
 btnDos.addEventListener("click", () => {
-console.log('este descifra');
-mensajeCip.value = mensajeDescip.value.innerText;
+  let mensajeDescip = document.getElementById("cuadroTextoDescifrado").value;
+  let espacioAMover = document.getElementById("numInput").value; 
+  let deco = window.cipher.decode(mensajeDescip, espacioAMover);
+  mensajeCip = mensajeDescip.innerText;
+  console.log(deco);
+  //mensajeCip.value = mensajeDescip.value.innerText;
 });
 
 }
