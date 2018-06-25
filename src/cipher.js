b1680273296a3fe2381180fac7fee795c44345ae
Kaybel <kblmrtnz@gmail.com>
2018-06-25 13:01:17 -0400

    se solventa error en decode pero sigue apareciendo NaN en console.log

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,7 @@
 window.cipher = {
   encode : (cifradociph, espacioAMover) => {
     let cifrado = cifradociph;
-    let result = "";
+    let result = 0;
     for(let i = 0; i < cifrado.length; i++){
       // se aplica from char code para que retorno los espacios del char code at
    let infoCip = ((cifrado.charCodeAt(i) + 65 - (espacioAMover)) % 26 + 65);
@@ -11,11 +11,11 @@ window.cipher = {
   },
   decode : (descifradociph, espacioAMover) => {
     let descifrado = descifradociph;
-    let result2 = "";
-    for(let i = 0; i < descifrado.length; i++){
+    let result2 = 0;
+    for(let j = 0; j < descifrado.length; j++){
       // se aplica from char code para que retorno los espacios del char code at
-   let infoDec = ((descifrado.fromCharCode(i) - 65 + (espacioAMover)) % 26 - 65);
-  result2 += descifrado.fromCharCode(infoDec);
+   let infoDec = ((descifrado.charCodeAt(j) - 65 + (espacioAMover)) % 26 - 65);
+  result2 += descifrado.charCodeAt(infoDec);
   }
   return result2;  
   }
--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
 window.onload = function() { 
   // variables de botones 
-const btnUno = document.getElementById("btnId1");
-const btnDos = document.getElementById("btnId2");
+  const btnUno = document.getElementById("btnId1");
+  const btnDos = document.getElementById("btnId2");
 
 // evento DOM 
 
 // llamar funciones encode y decode
 
-btnUno.addEventListener("click", () => {
-  let mensajeCip = document.getElementById("cuadroTextoCifrado").value;
-  let espacioAMover = document.getElementById("numInput").value; 
-  let cif = window.cipher.encode(mensajeCip, espacioAMover);
-  let str = cif.toString();
-  mensajeDescip = mensajeCip.innerText;
-  console.log(str);
-  // mensajeDescip.value = mensajeCip.value.innerText;
-});
+  btnUno.addEventListener("click", () => {
+    let mensajeCip = document.getElementById("cuadroTextoCifrado").value;
+    let espacioAMover = document.getElementById("numInput").value; 
+    let cif = window.cipher.encode(mensajeCip, espacioAMover);
+    let str = cif;
+      if(str.text=="NaN"){
+        str.text=mensajeCip;
+      }
+    mensajeDescip = mensajeCip.innerText;
+    console.log(str);
+    // mensajeDescip.value = mensajeCip.value.innerText;
+  });
 
-btnDos.addEventListener("click", () => {
-  let mensajeDescip = document.getElementById("cuadroTextoDescifrado").value;
-  let espacioAMover = document.getElementById("numInput").value; 
-  let deco = window.cipher.decode(mensajeDescip, espacioAMover);
-  mensajeCip = mensajeDescip.innerText;
-  console.log(deco);
-  //mensajeCip.value = mensajeDescip.value.innerText;
-});
+  btnDos.addEventListener("click", () => {
+    let mensajeDescip = document.getElementById("cuadroTextoDescifrado").value;
+    let espacioAMover = document.getElementById("numInput").value; 
+    let deco = window.cipher.decode(mensajeDescip, espacioAMover);
+    let strDecodificada = deco;
+      if(strDecodificada.text=="NaN"){
+        strDecodificada.text=mensajeDescip;
+      }
+    mensajeCip = mensajeDescip.innerText;
+    console.log(strDecodificada);
+    //mensajeCip.value = mensajeDescip.value.innerText;
+  });
 
 }
